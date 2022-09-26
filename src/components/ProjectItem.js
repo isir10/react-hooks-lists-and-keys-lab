9d777b1b4916cc3ad0b7f7b311d75b5010927398
isir10 <isir.malak@moringaschool.com>
2022-09-26 17:35:05 +0300

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
-    </div>
-  );
+	return (
+		<div className="project-item">
+			<h3>{name}</h3>
+			<p>{about}</p>
+			<div className="technologies">
+				{technologies.map((tecno) => (
+					<span key={tecno}>{tecno}</span>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default ProjectItem;
